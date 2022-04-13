0414534043a47e3ead453d41917e1708e3975687
Hwanu Jung <sossont1@gmail.com>
2022-04-13 13:05:20 +0900

    Chore : Redis 설정

---
+++ src/main/java/com/ssinhwa/gameserver/chatserver/config/RedisConstants.java
@@ -0,0 +1,2 @@
+package com.ssinhwa.gameserver.chatserver.config;public class RedisConstants {
+}
---
+++ src/main/java/com/ssinhwa/gameserver/chatserver/service/RedisSubscriber.java
@@ -0,0 +1,2 @@
+package com.ssinhwa.gameserver.chatserver.service;public class RedisSubscriber {
+}
