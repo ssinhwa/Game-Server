94b5ce128be8787736e91112fc59fb1c0ba4039c
Hwanu Jung <sossont1@gmail.com>
2022-11-01 00:11:11 +0900

    Feat : ConcurrentModificationException Error 해결

--- src/main/java/com/ssinhwa/gameserver/redisserver/repository/PeriodicDataRepositoryImpl.java
+++ src/main/java/com/ssinhwa/gameserver/redisserver/repository/PeriodicDataRepositoryImpl.java
@@ -5,27 +5,31 @@ import com.google.gson.JsonElement;
 import com.ssinhwa.gameserver.redisserver.entity.PeriodicData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 @Slf4j
 public class PeriodicDataRepositoryImpl implements PeriodicDataRepository {
-    private static final List<PeriodicData> memoryPeriodicDataRepository = new ArrayList<>();
+    private static final List<PeriodicData> memoryPeriodicDataRepository = Collections.synchronizedList(new ArrayList<>());
 
     @Override
+    @Transactional
     public void save(PeriodicData data) {
-        log.info("Periodic data save 완료");
         memoryPeriodicDataRepository.add(data);
     }
 
     @Override
+    @Transactional
     public JsonElement getAll() {
         return new Gson().toJsonTree(memoryPeriodicDataRepository);
     }
 
     @Override
+    @Transactional
     public void deleteAll() {
         memoryPeriodicDataRepository.clear();
     }
--- src/main/java/com/ssinhwa/gameserver/redisserver/repository/PlayerContinuousDataRepositoryImpl.java
+++ src/main/java/com/ssinhwa/gameserver/redisserver/repository/PlayerContinuousDataRepositoryImpl.java
@@ -3,26 +3,33 @@ package com.ssinhwa.gameserver.redisserver.repository;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.ssinhwa.gameserver.redisserver.entity.PlayerContinuousData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
+@Slf4j
 public class PlayerContinuousDataRepositoryImpl implements PlayerContinuousDataRepository {
-    private static final List<PlayerContinuousData> memoryPlayerContinuousDataRepository = new ArrayList<>();
+    private static final List<PlayerContinuousData> memoryPlayerContinuousDataRepository = Collections.synchronizedList(new ArrayList<>());
 
     @Override
+    @Transactional
     public void save(PlayerContinuousData data) {
         memoryPlayerContinuousDataRepository.add(data);
     }
 
     @Override
+    @Transactional
     public JsonElement getAll() {
         return new Gson().toJsonTree(memoryPlayerContinuousDataRepository);
     }
 
 
+    @Transactional
     public void deleteAll() {
         memoryPlayerContinuousDataRepository.clear();
     }
